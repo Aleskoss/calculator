1a1495a0de08940768cded007bcadee98efd8a8f
Aleskoss <187804686+Aleskoss@users.noreply.github.com>
2025-01-22 20:22:56 +0100

    Create add,substract,multiply,divide and operate functions

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,34 @@
-function add(...num){
-
+function add(num1,num2){
+    return num1 + num2
 }
 
-function substract(...num){
-
+function substract(num1,num2){
+    return num1 - num2
 }
 
-function multiply(...num){
-
+function multiply(num1,num2){
+    return num1 * num2
 }
 
-function divide(...num){
-    
+function divide(num1,num2){
+    return num1/num2
+}
+
+function operate(operator,num1,num2){
+    let sum = 0
+    switch(operator){
+        case `+`:
+            sum = add(num1,num2)
+            break
+        case `-`:
+            sum = substract(num1,num2)
+            break
+        case `*`:
+            sum = multiply(num1,num2)
+            break
+        case `/`:
+            sum = divide(num1,num2)
+            break
+    }
+    console.log(sum)
 }
