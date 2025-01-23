28ab722a6410270d23dc77995f35da85fd477fab
Aleskoss <187804686+Aleskoss@users.noreply.github.com>
2025-01-23 19:47:28 +0100

    Make the calculator work by calling the function operate through buttons

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
 function add(num1,num2){
-    return num1 + num2
+    return +num1 + +num2
 }
 
 function substract(num1,num2){
@@ -30,25 +30,37 @@ function operate(operator,num1,num2){
             sum = divide(num1,num2)
             break
     }
-    console.log(sum)
+    return sum
 }
-
 
 function manipulateDom(){
     let operator = 0
     let num1 = 0
     let num2 = 0
+    let sum = 0
     const display = document.querySelector(`#display`)
     const [...buttons] = document.querySelectorAll(`button`)
-    console.log(buttons)
-    buttons.forEach(item => addEventListener(`click`,(event) => {
-        if(num1 !== 0 && operator !== 0){
-            num2 = event.target.innerHTML
+    buttons.forEach(item => item.addEventListener(`click`,(event) => {
+        if(event.target.textContent == `=`){
+            sum = operate(operator,num1,num2)
+            display.textContent = `${sum}`
+            num1 = sum
+            num2 = 0
+            operator = 0
+        }else if(num1 !== 0 && operator !== 0){
+            num2 = event.target.textContent
+            display.textContent = event.target.textContent
         }else if(num1 !== 0){
-            operator = event.target.innerHTML
+            operator = event.target.textContent
+            display.textContent = event.target.textContent
+        }else if(num1 === 0){
+            num1 = event.target.textContent
+            display.textContent = event.target.textContent
+        }else if(event.target.textContent === `clear`){
+            num1 = 0
+            num2 = 0
+            operator = 0
         }
-            num1 = event.target.innerHTML
     }))
-    console.log(num1)
 }
 manipulateDom()
