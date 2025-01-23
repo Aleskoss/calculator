264d3a61f3164ef03e7a475dc1c486320be42bbf
Aleskoss <187804686+Aleskoss@users.noreply.github.com>
2025-01-23 21:00:42 +0100

    Change inputed numbers to array user can input numbers with more digits

--- javascript/script.js
+++ javascript/script.js
@@ -35,28 +35,31 @@ function operate(operator,num1,num2){
 
 function manipulateDom(){
     let operator = 0
-    let num1 = 0
-    let num2 = 0
+    let num1 = []
+    let num2 = []
     let sum = 0
     const display = document.querySelector(`#display`)
     const [...buttons] = document.querySelectorAll(`button`)
     buttons.forEach(item => item.addEventListener(`click`,(event) => {
-        if(event.target.textContent == `=`){
-            sum = operate(operator,num1,num2)
+        let targeted = event.target.textContent
+        if(targeted == `=`){
+            sum = operate(operator,num1.join(``),num2.join(``)).toFixed(5)
             display.textContent = `${sum}`
-            num1 = sum
-            num2 = 0
+            num1 = []
+            num1.push(sum)
+            num2 = []
             operator = 0
-        }else if(num1 !== 0 && operator !== 0){
-            num2 = event.target.textContent
-            display.textContent = event.target.textContent
-        }else if(num1 !== 0){
-            operator = event.target.textContent
-            display.textContent = event.target.textContent
-        }else if(num1 === 0){
-            num1 = event.target.textContent
-            display.textContent = event.target.textContent
-        }else if(event.target.textContent === `clear`){
+        }else if(operator !== 0 && num1.length !== 0){
+            num2.push(targeted)
+            display.textContent = num2.join(``)
+        }else if(targeted === `-` || targeted === `+` || targeted === `/` || targeted === `*`){
+            operator = targeted
+            display.textContent = targeted
+        }else if(num1.length === 0 || operator === 0){
+            num1.push(targeted)
+            display.textContent = num1.join(``)
+            console.log(num1)
+        }else if(targeted === `C`){
             num1 = 0
             num2 = 0
             operator = 0
