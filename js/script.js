6c87cf86754e87aa2b584dec6187c698e1e1b1ad
Aleskoss <187804686+Aleskoss@users.noreply.github.com>
2025-05-19 09:12:51 +0200

    Create operate function
    Operate function decides which operation to do

--- js/script.js
+++ js/script.js
@@ -10,3 +10,18 @@ function multiply(num1,num2){
 function divide(num1,num2){
   return num1 / num2
 }
+// const firstNumber = prompt("")
+// const operator = prompt(" ")
+// const secondNumber = prompt("")
+function operate(num1,operator,num2){
+  if(operator === "+"){
+    console.log(add(num1,num2))
+  }else if(operator === "-"){
+    subtract(num1,num2)
+  }else if(operator === "*"){
+    multiply(num1,num2)
+  }else if(operator === "/"){
+    divide(num1,num2)
+  }
+}
+operate(firstNumber,operator,secondNumber)
